3bc6bef0b5fbf74656ee77ad249466f26d53c378
swapnilnewvision <swapnil@sports-techsolutions.com>
2025-11-02 22:46:19 +0530

    fixed base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1/client';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1/client';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
